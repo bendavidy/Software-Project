4aecf7178f4e68ee671f188825e4f85fdc3ca5c8
Yoad <bendavidy2@gmail.com>
2025-12-30 22:57:45 +0000

    Implemented find_minimal_dist_index, organized code

--- kmeans.c
+++ kmeans.c
@@ -8,17 +8,17 @@
 // Y[X] read the input
 //  [ ] float convertion function
 // Y[X] variable initialization
-// Y[ ] find_minimal_dist_index function
+// Y[X] find_minimal_dist_index function
 // S[ ] update_assignment function
 // S[ ] update_centroids function
 //  [ ] main algorithm loop
 
-// Global variables declerations
+// --------------- Global variables declerations ---------------
 int K, iter, N, d;
+
 // struct vector input_data[];
 // each line is a linked list
 // input_data is a vector of those lists
-
 struct node
 {
     double value;
@@ -30,13 +30,47 @@ struct vector
     struct node *nodes;
 };
 
-// Function declerations (prototypes):
+struct vector **centroids; // centroids will point to the first elem of [vector*,vector*,..,vector*] after we will allocate space later..
+struct vector *head_vec;   // indicates the head vector of the input data
+
+// --------------- Function declerations (prototypes) and implementations: ---------------
 void update_assignments(void);
 void update_centroids(void);
-void find_minimal_dist_index(int i);
-struct node* deep_clone_nodes(struct node* node); // this function will take 
+int find_minimal_dist_index(struct vector *elem); // recieves a pointer to an element (represented by a vector) of dimension d, and returns the index of the closest centroid.
+struct node *deep_clone_nodes(struct node *node); // this function will take
 
-// Main
+int find_minimal_dist_index(struct vector *elem) // TODO: test
+{
+    double min = INFINITY;
+    int min_index;
+    struct vector *cent;
+
+    for (int j = 0; j < K; j++)
+    {
+        cent = centroids[j];
+        // calculate euclidean distance to centroid[j]
+        int sub, sum = 0;
+        struct node *curr_elem_coor = elem->nodes;
+        struct node *curr_cent_coor = cent->nodes;
+        for (int l = 0; l < d; l++)
+        {
+            sub = (curr_elem_coor->value - curr_cent_coor->value);
+            sub = sub * sub;
+            sum += sub;
+            curr_elem_coor = curr_elem_coor->next;
+            curr_cent_coor = curr_cent_coor->next;
+        }
+        sum = sqrt(sum);
+        if (sum < min)
+        {
+            min = sum;
+            min_index = j;
+        }
+    }
+    return min_index;
+}
+
+// --------------- Main ---------------
 int main(int argc, char *argv[])
 {
     // Read params and stdin
@@ -48,8 +82,6 @@ int main(int argc, char *argv[])
 
     struct vector *head_vec, *curr_vec, *next_vec;
     struct node *head_node, *curr_node, *next_node;
-
-    struct vector **centroids; // centroids will point to the first elem of [vector*,vector*,..,vector*] after we will allocate space later..
 
     int N = 0, d = 0;
     double n;
@@ -92,26 +124,23 @@ int main(int argc, char *argv[])
     N = (int)(N / d);
     iter = atoi(argv[2]);
     K = atoi(argv[1]);
-     // printf("%s%d, %s%d, %s%d, %s%d", "K = ", K, "iter = ", iter, "N = ", N, "d = ", d);
+    // printf("%s%d, %s%d, %s%d, %s%d", "K = ", K, "iter = ", iter, "N = ", N, "d = ", d);
 
-    centroids = malloc(K * sizeof(struct vector*)); // allocate K spaces, now centroids is pointing to the first elem of [vector*,vector*,..,vector*]
+    centroids = malloc(K * sizeof(struct vector *)); // allocate K spaces, now centroids is pointing to the first elem of [vector*,vector*,..,vector*]
     next_vec = head_vec;
-    int k;
 
-    for(k = 0; k < K; k++)
+    for (int k = 0; k < K; k++)
     {
         centroids[k] = malloc(sizeof(struct vector));
         centroids[k]->next = NULL;
         centroids[k]->nodes = deep_clone_nodes(next_vec->nodes);
 
-        next_vec = next_vec->next; 
+        next_vec = next_vec->next;
     }
 
-   
+    // --------------- main loop ---------------
 
-    // main loop
-
-    // free memory
+    // --------------- free memory ---------------
     curr_vec = head_vec;
     struct vector *temp_vec;
     struct node *temp_node;
