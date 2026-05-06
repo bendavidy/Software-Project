8372260a18a596578ec6a92fe6d1cd2333581e90
Yoad <bendavidy2@gmail.com>
2026-05-06 23:20:18 +0000

    Created Makerfile, Started working on symnmf.c main() function

--- Final-Project/symnmf.c
+++ Final-Project/symnmf.c
@@ -5,12 +5,16 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 /* --------------- Global and extern variables declerations --------------- */
-extern int N, K, iter, d;
-extern double eps;
-extern struct vector* head_vec;
+// TODO: this was set to avoid compilation error while testing. figure out how to do this when submitting
+int N = 0, K = 0, iter = 0, d = 0;
+double eps = 0;
+struct vector* head_vec;
+// extern int N, K, iter, d;
+// extern double eps;
+// extern struct vector* head_vec;
 
 double **A, **W;
 double* D;
@@ -20,7 +24,7 @@ double* D;
 
 /* --------------- Function declerations (prototypes) and implementations: --------------- */
 void* check_alloc(void* p);
-double euclidean_dist_vec(struct node* a, struct node* b);
+// double euclidean_dist_vec(struct node* a, struct node* b);
 double euclidean_dist_squared(double* a, double* b);
 void free_nodes(struct node* head);
 struct node* deep_clone_nodes(struct node* node); /* this function will take */
@@ -34,19 +38,19 @@ void* check_alloc(void* p)
     return p;
 }
 
-double euclidean_dist_vec(struct node* a, struct node* b) {
-    int l;
-    double sub, sum = 0.0;
-    for (l = 0; l < d; l++) {
-        sub = (a->value - b->value);
-        sub = sub * sub;
-        sum += sub;
-        a = a->next;
-        b = b->next;
-    }
-    sum = sqrt(sum);
-    return sum;
-}
+// double euclidean_dist_vec(struct node* a, struct node* b) {
+//     int l;
+//     double sub, sum = 0.0;
+//     for (l = 0; l < d; l++) {
+//         sub = (a->value - b->value);
+//         sub = sub * sub;
+//         sum += sub;
+//         a = a->next;
+//         b = b->next;
+//     }
+//     sum = sqrt(sum);
+//     return sum;
+// }
 
 double euclidean_dist_squared(double* a, double* b) {
     double sum = 0.0;
@@ -91,6 +95,14 @@ struct node* deep_clone_nodes(struct node* src_node) /* recieve a pointer to som
     return head;
 }
 
+int check_file_extension(char* filename, char* ext) {
+    char* dot = strrchr(filename, '.');
+    if (!dot || dot == filename) {
+        return 0;
+    }
+    return strcmp(dot + 1, ext) == 0;
+}
+
 void print_vector_nodes(struct node* p)
 // TODO: test this
 {
@@ -107,6 +119,18 @@ void print_vector_nodes(struct node* p)
     }
 }
 
+void print_double_matrix(double** mat)
+// TODO: test this
+{
+    for (int i = 0; i < N; i++) {
+        printf("%.4f", mat[i][0]);
+        for (int j = 1; j < N; j++) {
+            printf(",%.4f", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 // Calculating Frobenius norm (without the sqrt) of a NxK matrix
 double frob_squared(double** M) {
     double sum = 0;
@@ -117,7 +141,6 @@ double frob_squared(double** M) {
     }
     return sum;
 }
-
 double** sym(double** C_in) {
     A = check_alloc(malloc(N * sizeof(double*)));
     for (int i = 0; i < N; i++) {
@@ -217,10 +240,81 @@ double** symnmf(double** H, double** W) { // This W is unrelated to the global W
 
 // parse CMD arguments and print result based on goal
 // Defined in 2.2
-int main(int argc, char const *argv[])
-{
-    // TODO: implement
+int main(int argc, char* argv[]) {
+    struct vector *curr_vec, *next_vec; /*, *printed_vec;*/
+    struct node *head_node, *curr_node, *final_node; /* next_node; */
+    /* struct vector **centroids; centroids will point to the first elem of [vector*,vector*,..,vector*] after we will allocate space later.. */
+    double n;
+    char c;
+    struct vector* temp_vec;
+    int k;
+    int it;
+    char *end, *goal, *input_file;
+
+    if (argc != 3) {
+        printf("%s", "An Error Has Occurred\n");
+        exit(1);
+    } else {
+        goal = argv[1];
+        input_file = argv[2];
+        if (!check_file_extension(input_file, "txt")) {
+            printf("%s", "An Error Has Occurred\n");
+            exit(1);
+        }
+    }
+    // TODO: delete this - debug printing
+    printf("goal = %s, file is %s\n", goal, input_file);
+
+    // TODO: continue main function here:
+    // 0. validating "goal" input and printing an error otherwise
+    // 1. reading input data into the vector/node struct
+    // 2. allocating N,d
+    // 3. converting the vectors to a double** matrix (can use a variation of the same functino we wrote for PyObject)
+    // 4. call sym/ddg/norm function based on goal, and print the output
+    // 5. ddg print need extra care - printing it should include zeros everywhere except for the diagonal
+
+    // copied code from kmeans.c - use this
+    head_node = check_alloc(malloc(sizeof(struct node)));
+    curr_node = head_node;
+    curr_node->next = NULL;
+
+    head_vec = check_alloc(malloc(sizeof(struct vector)));
+    curr_vec = head_vec;
+    curr_vec->next = NULL;
+
+    while (scanf("%lf%c", &n, &c) == 2) {
+        N++;
+        if (c == '\n') {
+            curr_node->value = n;
+            curr_vec->nodes = head_node;
+            curr_vec->next = check_alloc(malloc(sizeof(struct vector)));
+            curr_vec = curr_vec->next;
+
+            curr_vec->next = NULL;
+            curr_vec->nodes = NULL; /*New line-Shalev*/
+
+            head_node = check_alloc(malloc(sizeof(struct node)));
+            curr_node = head_node;
+            curr_node->next = NULL;
+            if (d == 0) {
+                d = N;
+            }
+            continue;
+        }
+
+        curr_node->value = n;
+        curr_node->next = check_alloc(malloc(sizeof(struct node)));
+        curr_node = curr_node->next;
+        curr_node->next = NULL;
+    }
+    final_node = head_node;
+
+    N = (int)(N / d);
+    K = strtol(argv[1], &end, 10);
+    if ((end == argv[1]) || (*end != '\0') || (K <= 1) || (K >= N)) {
+        printf("%s", "Incorrect number of clusters!\n");
+        exit(1);
+    }
+
     return 0;
 }
-
-
